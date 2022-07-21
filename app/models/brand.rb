67fe03de055104109b2f53db39070fb3f886697c
Neha Nagurkar <neha.nagurkar@fitterfly.in>
2022-07-21 17:35:14 +0530

    Pages controller creation.

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,4 @@
 class Brand < ApplicationRecord
-
+    validates :brand_name, presence: true, length: {minimum: 4, maximum: 30} 
+    validates :origin_country, presence: true, length: {minimum: 4, maximum: 30}
 end
--- app/models/device.rb
+++ app/models/device.rb
@@ -1,2 +1,8 @@
 class Device < ApplicationRecord
+    validates :brand_name, presence: true, length: {minimum: 4,maximum: 30}
+    validates :device_name, presence: true, length: {minimum: 1,maximum: 30}
+    validates :screen_size, presence: true, length: {minimum: 3,maximum: 10}
+    validates :camera, presence: true, length: {minimum: 1,maximum: 10}
+    validates :battery, presence: true, length: {minimum: 3,maximum: 10}
+    validates :brand_id, presence: true, length: {minimum: 1,maximum: 5}
 end
